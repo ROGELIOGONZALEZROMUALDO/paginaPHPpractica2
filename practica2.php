63646eae2ca74332f4d27a74b8f57c8d3003b94c
ROGELIO GONZALEZ <rogeicogz@gmail.com>
2018-08-26 20:23:27 -0500

    Programacion de Paginas con PHP

---
+++ practica2.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+<title> TAREA PRACTICA #2</title>
+</head>
+<body style="background-color:#AED6F1;">
+<?php echo '<center><h1>TAREA 1: TIPOS DE DATOS EN PHP</h1></center>'; ?>
+<?php echo '<p>Los principales tipos de datos (considerados tipos de datos escalares) en PHP son:	</p>'; ?>
+<?php echo '<p>Declarar una variable en PHP</p>'; ?>
+<?php echo '<p> $nombredela_Variable = valor(numerico o cadena string). </p>'; ?>
+<?php echo '<p> boolean: almacenan valores verdadero o falso (true / false).  </p>'; ?>
+<?php echo '<p> integer: números enteros.  </p>'; ?>
+<?php echo '<p> float: números con decimales, usando el punto como separador decimal. </p>'; ?>
+<?php echo '<p> string: cadenas de texto </p>'; ?>
+<?php echo '<center><p>PHP también incluye los siguientes tipos de datos compuestos  </p></center>'; ?>
+<?php echo '<p> object: almacena objetos en PHP.  </p>'; ?>
+<?php echo '<p> array: usados para almacenar varios valores. </p>'; ?>
+<?php echo '<center><p> Incluye también los siguientes tipos de datos especiales:  </p></center>'; ?>
+<?php echo '<p> resource: hace referencia a recursos tales como archivos abiertos, conexiones establecidas con bases de datos, etc.  </p>'; ?>
+<?php echo '<p> null: indique el valor está vacío (no contiene nada). </p>'; ?>
+<?php echo '<center><p>  </p></center>'; ?>
+
+<?php echo '<center><h1>TAREA 2: OPERADORES EN PHP</h1></center>'; ?>
+<?php echo '<p> Un operador nos permite realizar una operación entre uno o más valores.
+ El operador toma esos valores de entrada y los relaciona entre si, realizando una operación y aplicando otro valor como resultado</p>'; ?>
+ 
+ <?php echo '<p> Operadores aritméticos </p>'; ?>
+ <?php echo '<p> Nos permiten realizar operaciones numéricas con nuestras variables.
+ Son los más fáciles de asimilar, porque generalmente todos conocemos esas operaciones. </p>'; ?>
+ <?php echo '<p> + Suma </p>'; ?>
+ <?php echo '<p> - Resta </p>'; ?>
+ <?php echo '<p> * Multiplicación </p>'; ?>
+ <?php echo '<p> / División </p>'; ?>
+ <?php echo '<p> % Módulo (resto de la división </p>'; ?>
+ <?php echo '<p> ** Exponenciación (2 ** 3, elevar 2 la a tercera potencia) </p>'; ?>
+ 
+ <?php echo '<p> Operadores de Asignación </p>'; ?>
+ <?php echo '<p> Los operadores de asignación son los más habituales y nos permiten traspasar valores en variables. 
+  Asignar es el proceso por el cual colocamos un valor en una variable. </p>'; ?>
+ <?php echo '<p> = Asignación </p>'; ?>
+ <?php echo '<p> += Suma y asignación </p>'; ?>
+ <?php echo '<p> -= Resta y asignación </p>'; ?>
+ <?php echo '<p> *= Multiplicación y asignación </p>'; ?>
+ <?php echo '<p> /= División y asignación </p>'; ?>
+ <?php echo '<p> %= Módulo y asignación</p>'; ?>
+ <?php echo '<p> == Comprueba si son iguales </p>'; ?>
+ <?php echo '<p> != Comprueba si son distintos </p>'; ?>
+ <?php echo '<p> === Comprueba si son iguales y de exactamente el mismo tipo </p>'; ?>
+ <?php echo '<p> !== Comprueba si son distintos o de distinto tipo </p>'; ?>
+ <?php echo '<p> <> Diferente (igual que !=) </p>'; ?>
+ <?php echo '<p> < Menor qué, comprueba si un valor es menor que otro </p>'; ?> 
+ <?php echo '<p> > Mayór qué </p>'; ?>
+ <?php echo '<p> <= Menor o igual </p>'; ?> 
+ <?php echo '<p> >= Mayor o igual </p>'; ?>
+ <?php echo '<p> <=> Comparador de orden. (PHP 7) </p>'; ?> 
+ <?php echo '<p> Logicos </p>'; ?>
+ <?php echo '<p> Los operadores lógicos sirven para realizar operaciones lógicas, valga la redundancia. 
+ Son operaciones que al final van a devolver un "sí" o un "no", positivo o negativo. 
+ Se usan en combinación con los operadores de comparación cuando la expresión de la condición lo requiere. </p>'; ?> 
+ <?php echo '<p> and Operación lógica "y", será verdadero si ambos son verdaderos. </p>'; ?>
+ <?php echo '<p> or Operación lógica "o", será verdadero si uno de ellos es verdadero. </p>'; ?> 
+ <?php echo '<p> xor Operación lógica "xor", será verdadero si uno de ellos es verdadero, pero no ambos. </p>'; ?>
+ <?php echo '<p> ! Operación de negación, si era verdadero pasa a falso y viceversa. </p>'; ?> 
+ <?php echo '<p> && Operación lógica "y" </p>'; ?>
+ <?php echo '<p> || Operación lógica "o" </p>'; ?>
+ 
+ <?php echo '<p> Operadores de incremento </p>'; ?>
+ <?php echo '<p> Sirven para aumentar o disminuir en una unidad el valor de una variable. 
+ Son atajos para hacer una operación típica en los recorridos de bucles, 
+ de aumentar o disminuir un valor que usamos para llevar la cuenta de las iteraciones realizadas. </p>'; ?>
+ <?php echo '<p> ++$a Pre-incremento </p>'; ?>
+ <?php echo '<p> $a++ Post-incremento </p>'; ?>
+ <?php echo '<p> --$a Pre-decremento </p>'; ?>
+ <?php echo '<p> $a-- Post-decremento </p>'; ?>
+ 
+ <?php echo '<p> Procedencia </p>'; ?>
+ <?php echo '<p> Concatenacion </p>'; ?>
+ 
+ <?php echo '<center><h1>TAREA 1: ARREGLOS EN PHP</h1></center>'; ?>
+ <?php echo '<p> Bien, antes de comenzar, tenemos que saber que en php existen 3 tipos diferentes de arreglos, los cuales son los siguientes. </p>'; ?>
+ <?php echo '<p> Arreglos numéricos: estos son los arreglos estandar a los que cuyas variables se accede por medio de un indice numerico. </p>'; ?>
+ <?php echo '<p> Arreglos asociativos: para acceder a las variables de estos arreglos es necesario utilizar un id, no necesariamente numérico. </p>'; ?>
+ <?php echo '<p> Arreglos multidimensionales: Son arreglos mas complejos que pueden contener arreglos dentro de arreglos. </p>'; ?>
+ <?php echo '<p> Ejemplo. </p>'; ?>
+ <?php echo '<p>  </p>'; ?>
+ <img src="C:\xampp\Paginas\img.gif">
+ <?php echo '<p>  </p>'; ?>
+ <?php echo '<p> ROGELIO GONZALEZ ROMUALDO </p>'; ?>
+</body>
+
+</html>
